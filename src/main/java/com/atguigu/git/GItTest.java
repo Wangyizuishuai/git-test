7908c0eb30d32ca0b38db5f0bcbb07257f638ce6
wangyi <1913726709@qq.com>
2022-11-02 22:53:21 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -3,5 +3,9 @@ package com.atguigu.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("hello word4");
+        System.out.println("hello word4");
+        System.out.println("hello word4");
+
     }
 }
