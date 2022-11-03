10c2fa1e5f27e4d107e9d74e425682e038f9a10b
Wangyizuishuai <117296752+Wangyizuishuai@users.noreply.github.com>
2022-11-03 19:21:55 +0800

    pull commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -7,6 +7,7 @@ public class GItTest {
         System.out.println("hello word4");
         System.out.println("hello word4");
         System.out.println("hello word4");
+        System.out.println("wyzuishuai");
 
     }
 }
